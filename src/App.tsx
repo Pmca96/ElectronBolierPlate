005cf3d68b183948a76bd7ce174a0afa18c00cfd
Pedro Alves <pedromca96@gmail.com>
2023-02-11 12:11:13 +0000

    Init electron + CRA + TS + antd + no template
    CRA
    Electron v 23.0
    React 18.2
    Node 18.13
    TypeScript 4.9
    Ants 5.2
    Electron Builder 23.6

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Button, DatePicker, Space, version } from 'antd';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>antd version: {version}</h1>
+        <Space>
+          <DatePicker />
+          <Button type="primary">Primary Button</Button>
+        </Space>
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
